e07f7470daac769574f01789a06bf9b7d5f8fca2
Joel Feinstein <joel@nyansa.com>
2018-07-12 00:31:44 -0700

    Add text support

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  twilioAccontSid: process.env.TWILIO_ACCOUNT_SID || null,
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN || null,
   // an optional security measure - if both are set, then recaptcha will be used.
   recaptchaSiteKey: process.env.RECAPTCHA_SITE || null,
   recaptchaSecretKey: process.env.RECAPTCHA_SECRET || null,
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 const crypto = require('crypto');
+const twilio = require('twilio');
+const config = require('../config');
+
+const twilioClient = new twilio(config.twilioAccontSid, config.twilioAuthToken);
+
+const textDelay = 15 * 60 * 1000; // ms
+let lastTextedAt = null;
 
 function randomValueHex (len) {
   // https://blog.tompawlak.org/generate-random-values-nodejs-javascript
@@ -16,6 +23,23 @@ router.get('/', function(req, res) {
 
 router.post('/halt', (req, res) => {
   res.render('halt', { txId: req.body.txId });
+
+  const now = new Date();
+
+  if (now - lastTextedAt >= textDelay) {
+    console.log('Sending text');
+
+    twilioClient.messages.create({
+      to: '+18583956393',
+      from: '+16193831195',
+      body: 'STOP PLAYING SAXOPHONE!'
+    });
+
+    lastTextedAt = now;
+  } else {
+    const timeLeft = textDelay - (now - lastTextedAt);
+    console.log(`Skipping text for ${timeLeft / 1000}s`);
+  }
 });
 
 router.post('/feedback', (req, res) => {

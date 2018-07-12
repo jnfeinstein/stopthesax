c288a81a88f193c633589e8948c9bd1bfc864171
Joel Feinstein <jnfeinstein@gmail.com>
2018-07-12 12:20:59 -0700

    Make fields required.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('input[type="radio"]').change(function() {
     $('#input_request').prop('disabled', $(this).val() !== 'request');
+    $('#input_request').prop('required', $(this).val() === 'request');
     $('#input_other').prop('disabled', $(this).val() !== 'other');
+    $('#input_other').prop('required', $(this).val() === 'other');
   });
 });
